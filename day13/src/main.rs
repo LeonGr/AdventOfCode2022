d8440f7ce8f3e299740d354cfef706717ef7b910
LeonGr <mail+github@leongr.nl>
2022-12-13 08:38:32 +0100

    Slightly clean up day13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{cmp::Ordering, str::FromStr};
 
 fn read_input() -> String {
     let input = include_str!("../input");
@@ -13,11 +13,7 @@ enum Packet {
 
 impl Ord for Packet {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match is_right_order(self, other) {
-            Order::Correct => std::cmp::Ordering::Less,
-            Order::Equal => std::cmp::Ordering::Equal,
-            Order::Incorrect => std::cmp::Ordering::Greater,
-        }
+        get_order(self, other)
     }
 }
 
@@ -37,31 +33,7 @@ impl PartialEq for Packet {
     }
 }
 
-impl Eq for Packet {
-
-}
-
-impl ToString for Packet {
-    fn to_string(&self) -> String {
-        match self {
-            Packet::List(list) => {
-                let mut x = String::new();
-                x += "[";
-                for packet in list {
-                    x += &packet.to_string();
-                    x += ",";
-                }
-                if !list.is_empty() {
-                    x.pop();
-                }
-                x += "]";
-
-                x
-            },
-            Packet::Item(d) => d.to_string(),
-        }
-    }
-}
+impl Eq for Packet {}
 
 impl FromStr for Packet {
     type Err = String;
@@ -71,170 +43,136 @@ impl FromStr for Packet {
 
         let mut i = 0;
         let chars: Vec<char> = string.chars().collect();
-        loop {
-            match chars.get(i) {
-                Some(c) => {
-                    match c {
-                        ',' => (),
-                        '[' | ']' => {
-                            let x = c.to_string();
-                            parts.push(x);
-                        },
-                        d => {
-                            parts.push(d.to_string());
+        while let Some(c) = chars.get(i) {
+            match c {
+                ',' => (),
+                '[' | ']' => {
+                    let x = c.to_string();
+                    parts.push(x);
+                }
+                d => {
+                    parts.push(d.to_string());
 
-                            let mut j = i + 1;
-                            while let Some(y) = chars.get(j) {
-                                if y.is_numeric() {
-                                    let mut last = parts.pop().unwrap();
-                                    last += &y.to_string();
-                                    parts.push(last);
-                                } else {
-                                    break;
-                                }
-
-                                j += 1;
-                            }
-
-                            i = j - 1;
+                    let mut j = i + 1;
+                    while let Some(y) = chars.get(j) {
+                        if y.is_numeric() {
+                            let mut last = parts.pop().unwrap();
+                            last += &y.to_string();
+                            parts.push(last);
+                        } else {
+                            break;
                         }
+
+                        j += 1;
                     }
-                },
-                None => break,
+
+                    i = j - 1;
+                }
             }
 
             i += 1;
         }
 
-        println!("parts: {:?}", parts);
-
         let mut stack: Vec<Packet> = vec![];
 
-        parts.iter().for_each(|c| {
-            match c.to_string().as_str() {
-                "[" => {
-                    stack.push(Packet::List(vec![]));
-                }
-                "]" => {
-                    let packet = stack.pop().unwrap();
-                    if let Some(last_packet) = stack.last_mut() {
-                        match last_packet {
-                            Packet::List(packet_items) => packet_items.push(packet),
-                            Packet::Item(_) => unreachable!(),
-                        }
-                    } else {
-                        stack.push(packet);
-                    }
-                }
-                d => {
-                    let num: u8 = d.parse().unwrap();
-                    let last = stack.last_mut().unwrap();
-                    match last {
-                        Packet::List(packet_items) => packet_items.push(Packet::Item(num)),
+        parts.iter().for_each(|c| match c.to_string().as_str() {
+            "[" => {
+                stack.push(Packet::List(vec![]));
+            }
+            "]" => {
+                let packet = stack.pop().unwrap();
+                if let Some(last_packet) = stack.last_mut() {
+                    match last_packet {
+                        Packet::List(packet_items) => packet_items.push(packet),
                         Packet::Item(_) => unreachable!(),
                     }
+                } else {
+                    stack.push(packet);
+                }
+            }
+            d => {
+                let num: u8 = d.parse().unwrap();
+                let last = stack.last_mut().unwrap();
+                match last {
+                    Packet::List(packet_items) => packet_items.push(Packet::Item(num)),
+                    Packet::Item(_) => unreachable!(),
                 }
             }
         });
 
-        let packet = stack.pop().unwrap();
-        println!("s: {}", string);
-        println!("t: {}", packet.to_string());
-        assert_eq!(string, packet.to_string());
-
-        Ok(packet)
+        Ok(stack.pop().unwrap())
     }
 }
 
 type Pair = (Packet, Packet);
 
 fn parse(input: &str) -> Vec<Pair> {
-    let test: Vec<Pair> = input.split("\n\n")
+    input
+        .split("\n\n")
         .map(|pair| {
-            let parts: Vec<Packet> = pair.trim().split('\n').map(|packet| {
-                let packet: Packet = Packet::from_str(packet).unwrap();
-                packet
-            })
-            .collect();
+            let parts: Vec<Packet> = pair
+                .trim()
+                .split('\n')
+                .map(|packet| Packet::from_str(packet).unwrap())
+                .collect();
 
-            // println!();
             (parts[0].clone(), parts[1].clone())
         })
-        .collect();
-
-    test
+        .collect()
 }
 
-#[derive(Debug)]
-enum Order {
-    Correct,
-    Equal,
-    Incorrect,
-}
-
-fn is_right_order(first: &Packet, second: &Packet) -> Order {
-    println!("Compare:\n'{:?}'\n##VS##\n'{:?}'\n", first, second);
+fn get_order(first: &Packet, second: &Packet) -> Ordering {
     match (first, second) {
         (Packet::List(first_items), Packet::List(second_items)) => {
-            let max_len = usize::max(first_items.len(), second_items.len());
-
-            for i in 0..max_len {
+            for i in 0..(usize::max(first_items.len(), second_items.len())) {
                 match (first_items.get(i), second_items.get(i)) {
-                    (None, None) => return Order::Correct,
-                    (None, Some(_)) => return Order::Correct,
-                    (Some(_), None) => return Order::Incorrect,
-                    (Some(left), Some(right)) => {
-                        match is_right_order(left, right) {
-                            Order::Correct => return Order::Correct,
-                            Order::Equal => continue,
-                            Order::Incorrect => return Order::Incorrect,
-                        }
+                    (None, None | Some(_)) => return Ordering::Less,
+                    (Some(_), None) => return Ordering::Greater,
+                    (Some(left), Some(right)) => match get_order(left, right) {
+                        Ordering::Less => return Ordering::Less,
+                        Ordering::Equal => continue,
+                        Ordering::Greater => return Ordering::Greater,
                     },
                 }
             }
 
-            Order::Equal
-        },
+            Ordering::Equal
+        }
         (left @ Packet::List(_), Packet::Item(d)) => {
             let list_single_item = Packet::List(vec![Packet::Item(*d)]);
-            is_right_order(left, &list_single_item)
-        },
+            get_order(left, &list_single_item)
+        }
         (Packet::Item(d), right @ Packet::List(_)) => {
             let list_single_item = Packet::List(vec![Packet::Item(*d)]);
-            is_right_order(&list_single_item, right)
-        },
-        (Packet::Item(left), Packet::Item(right)) => {
-            match (left, right) {
-                (l, r) if l == r => Order::Equal,
-                (l, r) if l < r => Order::Correct,
-                (l, r) if l > r => Order::Incorrect,
-                _ => unreachable!(),
-            }
+            get_order(&list_single_item, right)
+        }
+        (Packet::Item(left), Packet::Item(right)) => match (left, right) {
+            (l, r) if l == r => Ordering::Equal,
+            (l, r) if l < r => Ordering::Less,
+            (l, r) if l > r => Ordering::Greater,
+            _ => unreachable!(),
         },
     }
 }
 
 fn part1(pairs: &[Pair]) -> usize {
-    pairs.iter().enumerate()
-        .filter(|(i, (first, second))| {
-            // println!("{:?}", (first, second));
-            println!("Pair {}", i + 1);
-            let test = is_right_order(first, second);
-            println!("right? {:?}\n", test);
-            match test {
-                Order::Correct => true,
-                Order::Equal => unreachable!(),
-                Order::Incorrect => false,
-            }
+    pairs
+        .iter()
+        .enumerate()
+        .filter(|(_, (first, second))| match get_order(first, second) {
+            Ordering::Less => true,
+            Ordering::Equal => unreachable!(),
+            Ordering::Greater => false,
         })
         .map(|(i, _)| i + 1)
         .sum()
 }
 
 fn part2(pairs: &[Pair]) -> usize {
-    let mut packets: Vec<Packet> = pairs.iter().flat_map(|(left, right)| {
-        vec![left.clone(), right.clone()]
-    }).collect();
+    let mut packets: Vec<Packet> = pairs
+        .iter()
+        .flat_map(|(left, right)| vec![left.clone(), right.clone()])
+        .collect();
 
     let first_divider = Packet::List(vec![Packet::List(vec![Packet::Item(2)])]);
     let second_divider = Packet::List(vec![Packet::List(vec![Packet::Item(6)])]);
@@ -244,19 +182,23 @@ fn part2(pairs: &[Pair]) -> usize {
 
     packets.sort();
 
-    packets.iter().enumerate().filter_map(|(i, packet)| {
-        if *packet == first_divider || *packet == second_divider {
-            Some(i + 1)
-        } else {
-            None
-        }
-    }).product()
+    packets
+        .iter()
+        .enumerate()
+        .filter_map(|(i, packet)| {
+            if *packet == first_divider || *packet == second_divider {
+                Some(i + 1)
+            } else {
+                None
+            }
+        })
+        .product()
 }
 
 fn main() {
     let input = read_input();
     let parsed = parse(&input);
 
-    println!("part1: {}", part1(&parsed));
-    println!("part2: {}", part2(&parsed));
+    println!("part1: {} should be 5330", part1(&parsed));
+    println!("part2: {} should be 27648", part2(&parsed));
 }
